305df37bbb7a9fecaa08668e10bc706ab3121e93
Atugonza Louis-Marie <atugonzalouis@gmail.com>
2026-06-16 09:43:16 +0300

    Create App.js

---
+++ frontend/frontend/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Study group finder system</h1>
+      <p>Campus Service Request API is running</p>
+    </div>
+  );
+}
+
+export default App;
